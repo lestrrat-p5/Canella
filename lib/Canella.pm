d86979cbbf7ef1bf0a439f5513c07078a48a867e
Daisuke Maki <lestrrat+github@gmail.com>
2013-07-04 21:14:24 +0900

    Add docs

--- lib/Canella.pm
+++ lib/Canella.pm
@@ -80,7 +80,42 @@ Based on the config file shown in SYNOPSIS, you can invoke commands like so:
 
 =head1 DESCRIPTION
 
-Canella is a simple deploy tool, based on L<Cinnamon>
+WARNING: ALPHA QUALITY CODE!
+
+Canella is yet another deploy tool, based on L<Cinnamon>
+
+=head1 DIFFERENCES WITH Cinnamon 0.22
+
+=over 4
+
+=item Goals
+
+Cinnamon wants to be "simple". Canella wants to be I<extendable> and 
+I<bendable>. Canella project just started, so it may not be completely there
+yet, but hopefully it will eventually get there
+
+Cinnamon stores state in class-variables which are sorta globals, and stuff
+like C<roles> and C<tasks> are stored has hashrefs. Backend fo Canella is 
+completely OO ala C<Moo>, which I believe is much easier to extend.
+
+You can (or you should be able to, if the current code is still not there)
+easily subclass Canella and bend it to do what you want.
+
+=item Supports multiple tasks
+
+Cinnamon 0.22 does not support specifying multiple tasks in one invocation.
+With Canella you can do
+
+    canella ... task1 task2 task2
+
+=item Concurrency works
+
+Cinnamon 0.22 has a broken concurrency problem where some tasks are
+repeatedly run against the same host.
+
+=head1 SEE ALSO
+
+L<Cinnamon>
 
 =head1 LICENSE
 
